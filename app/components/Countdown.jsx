5b9831ab1e6929a664b12de1fa36ee169233d947
Phillip Eade <Phillip Eade>
2017-02-09 14:45:16 +1100

    Update status to stop when count reaches 0

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -25,14 +25,23 @@ componentDidUpdate: function(prevProps, prevState){
     }
   }
 },
+
 startTimer: function(){
   this.timer = setInterval(() => {
     var newCount = this.state.count - 1;
     this.setState({
       count : newCount >= 0 ? newCount : 0
     });
+    if (newCount === 0){
+      this.setState({countdownStatus: 'stopped'});
+    }
   }, 1000);
 },
+componentWillUnmount: function(){
+  clearInterval(this.timer);
+  this.timer = undefined;
+},
+
 handleSetCountdown: function(seconds){
   this.setState({
     count: seconds,
