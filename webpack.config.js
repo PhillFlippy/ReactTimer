ded9130fca315e2682abab56f773896ca7c57b90
Phillip Eade <Phillip Eade>
2017-02-09 14:16:22 +1100

    Wired up controls for the stop watch

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ resolve: {
     Countdown: 'app/components/Countdown.jsx',
       Timer: 'app/components/Timer.jsx',
       Clock: 'app/components/Clock.jsx',
+      Controls: 'app/components/Controls.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
     applicationStyles: 'app/styles/app.scss'
   },
